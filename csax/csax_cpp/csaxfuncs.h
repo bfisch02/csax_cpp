1720ce357a04b979a84e89f2ad95cb034e2f67b1
Brett J. Fischler <bfisch02@dell24.eecs.tufts.edu>
2015-04-19 12:48:03 -0400

    Added csaxfuncs, a suite of functions to help with the csax algorithm

---
+++ csax/csax_cpp/csaxfuncs.h
@@ -0,0 +1,10 @@
+// Brett Fischler and Jacob Gerace
+// April 2015
+// Helper function declarations for the CSAX algorithm
+
+#include "sample.h"
+using namespace std;
+
+struct GeneScorePair;
+
+void runCSAX(vector<Sample> traindata, vector<Sample> testdata, int numBags);
